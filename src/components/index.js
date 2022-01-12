04af96e956678bb361f11a8fb8e0b138a7b66397
kvl-ballester <kvl.ballester@gmail.com>
2022-01-12 15:38:23 +0100

    add button component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import {Header} from "./Header/Header";
 import { MemberList } from "./MemberList/MemberList";
+import { Button } from "./Button/Button";
 
-export {Header, MemberList}
+export {Header,Button , MemberList}
