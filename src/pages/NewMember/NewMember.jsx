f03330464ead88437fc8f2dcc311db6d2a2be6bf
kvl-ballester <kvl.ballester@gmail.com>
2022-01-13 18:51:09 +0100

    set new member page layout

--- src/pages/NewMember/NewMember.jsx
+++ src/pages/NewMember/NewMember.jsx
@@ -1,13 +1,36 @@
-import { Header, BackTo } from "../../components";
+import { Header, BackTo, Button } from "../../components";
+import { useState } from 'react'
+import { MemberForm } from "./MemberForm";
+
+import "./newMember.scss"
 
 export const NewMember = () => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [sex, setSex] = useState("")
+    const [age, setAge] = useState(null)
+
+    const handleDeleteMember = () => {
+        console.log("click cancel")
+    }
+
+    const handleAddMember = () => {
+        console.log("click add member")
+    }
+
     return (
         <>  
             <Header text="Add member" />
             <div id="new-member">
                 <BackTo path="/" text="Back to members" />
-                <br />
-                Yluego un formulario
+                <div className="member-data">
+                    <MemberForm />
+                </div>
+                <div className="buttons">
+                    <Button text="Cancel" onClickButton={() => handleDeleteMember()}/>
+                    <Button text="Add" onClickButton={() => handleAddMember()}/>
+
+                </div>
             </div>
         </>
         
