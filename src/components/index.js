469dfd35a4d6d30377cd272575a6944ac1b34465
kvl-ballester <kvl.ballester@gmail.com>
2022-01-13 12:45:50 +0100

    create back arrow component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import {Header} from "./Header/Header";
 import { MemberList } from "./MemberList/MemberList";
 import { Button } from "./Button/Button";
+import { BackTo } from "./BackTo/BackTo";
 
-export {Header,Button , MemberList}
+export {Header,Button , MemberList, BackTo}
